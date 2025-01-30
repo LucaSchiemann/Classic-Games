dd9f7384bf57342b47015321c89aaf6770e68ade
LucaSchiemann <luca.n.schiemann@icloud.com>
2025-01-30 14:46:49 -0500

    Update and rename main.khanjs to main.js

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+//config for html
+
+var programCode = function(processingInstance) {
+    with (processingInstance) {
+      size(400, 400); 
+      frameRate(30);
+        
+      // Paste code from Khan Academy here:
+      fill(255, 255, 0);
+      ellipse(200, 200, 200, 200);
+      noFill();
+      stroke(0, 0, 0);
+      strokeWeight(2);
+      arc(200, 200, 150, 100, 0, PI);
+      fill(0, 0, 0);
+      ellipse(250, 200, 10, 10);
+      ellipse(153, 200, 10, 10);
+    }};
+
+  var canvas = document.getElementById("mycanvas"); 
+  var processingInstance = new Processing(canvas, programCode); 
+
+
+
+
+
+
+
+
 
 //Setting up main game
 
