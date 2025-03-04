5718fa81175c5c1bc24b2dd66637810e7c7f8a6a
Mygo2 <27rushha@gowcs.net>
2025-03-03 21:32:59 -0500

    added rudimentary game, should be cleaner code as well

--- checkers.js
+++ checkers.js
@@ -11,14 +11,11 @@ var checkerSize = 62.5;
 var row = 0;
 var column = 0;
 var reset = true;
-var darkCheckersX = [];
-var lightCheckersX = [];
-var emptySpacesX = [];
-var darkCheckersY = [];
-var lightCheckersY = [];
-var emptySpacesY = [];
 var selected = null;
+var selectedColumn = null;
+var selectedRow = null;
 var selectedColor = null;
+var turn = "black";
 
 //gameboards
 var board = [[["D", 0, 0, 37.5, 37.5, "light"], ["_", 75, 0, null, null, "dark"], ["D", 150, 0, 187.5, 37.5, "light"], ["_", 225, 0, null, null, "dark"], ["D", 300, 0, 337.5, 37.5, "light"], ["_", 375, 0, null, null, "dark"], ["D", 450, 0, 487.5, 37.5, "light"], ["_", 525, 0, null, null, "dark"]], [["_", 0, 75, null, null, "dark"], ["D", 75, 75, 112.5, 112.5, "light"], ["_", 150, 75, null, null, "dark"], ["D", 225, 75, 262.5, 112.5, "light"], ["_", 300, 75, null, null, "dark"], ["D", 375, 75, 412.5, 112.5, "light"], ["_", 450, 75, null, null, "dark"], ["D", 525, 75, 562.5, 112.5, "light"]], [["D", 0, 150, 37.5, 187.5, "light"], ["_", 75, 150, null, null, "dark"], ["D", 150, 150, 187.5, 187.5, "light"], ["_", 225, 150, null, null, "dark"], ["D", 300, 150, 337.5, 187.5, "light"], ["_", 375, 150, null, null, "dark"], ["D", 450, 150, 487.5, 187.5, "light"], ["_", 525, 150, null, null, "dark"]], [["_", 0, 225, null, null, "dark"], ["_", 75, 225, 112.5, 262.5, "light"], ["_", 150, 225, null, null, "dark"], ["_", 225, 225, 262.5, 262.5, "light"], ["_", 300, 225, null, null, "dark"], ["_", 375, 225, 412.5, 262.5, "light"], ["_", 450, 225, null, null, "dark"], ["_", 525, 225, 562.5, 262.5, "light"]], [["_", 0, 300, 37.5, 337.5, "light"], ["_", 75, 300, null, null, "dark"], ["_", 150, 300, 187.5, 337.5, "light"], ["_", 225, 300, null, null, "dark"], ["_", 300, 300, 337.5, 337.5, "light"], ["_", 375, 300, 412.5, 337.5, "dark"], ["_", 450, 300, 487.5, 337.5, "light"], ["_", 525, 300, null, null, "dark"]], [["_", 0, 375, null, null, "dark"], ["L", 75, 375, 112.5, 412.5, "light"], ["_", 150, 375, null, null, "dark"], ["L", 225, 375, 262.5, 412.5, "light"], ["_", 300, 375, null, null, "dark"], ["L", 375, 375, 412.5, 412.5, "light"], ["_", 450, 375, null, null, "dark"], ["L", 525, 375, 562.5, 412.5, "light"]], [["L", 0, 450, 37.5, 487.5, "light"], ["_", 75, 450, null, null, "dark"], ["L", 150, 450, 187.5, 487.5, "light"], ["_", 225, 450, null, null, "dark"], ["L", 300, 450, 337.5, 487.5, "light"], ["_", 375, 450, null, null, "dark"], ["L", 450, 450, 487.5, 487.5, "light"], ["_", 525, 450, null, null, "dark"]], [["_", 0, 525, null, null, "dark"], ["L", 75, 525, 112.5, 562.5, "light"], ["_", 150, 525, null, null, "dark"], ["L", 225, 525, 262.5, 562.5, "light"], ["_", 300, 525, null, null, "dark"], ["L", 375, 525, 412.5, 562.5, "light"], ["_", 450, 525, null, null, "dark"], ["L", 525, 525, 562.5, 562.5, "light"]], ];
@@ -31,10 +28,6 @@ while (row < 8){
             fill(color3);
             noStroke();
             rect(board[row][column][1], board[row][column][2], 75, 75);
-            if (board[row][column][0] === "_"){
-                emptySpacesX.push(board[row][column][1]);
-                emptySpacesY.push(board[row][column][2]);
-            }
         }
             
         else if (board[row][column][5] === "dark"){
@@ -51,8 +44,6 @@ while (row < 8){
             strokeWeight(4);
             stroke(color2);
             ellipse(board[row][column][3], board[row][column][4], checkerSize/1.2,checkerSize/1.2);
-            darkCheckersX.push(board[row][column][3]);
-            darkCheckersY.push(board[row][column][4]);
         }
         
         else if (board[row][column][0] === "L"){
@@ -63,8 +54,6 @@ while (row < 8){
             strokeWeight(4);
             stroke(color6);
             ellipse(board[row][column][3], board[row][column][4], checkerSize/1.2,checkerSize/1.2);
-            lightCheckersX.push(board[row][column][3]);
-            lightCheckersY.push(board[row][column][4]);
         }
         
         else{
@@ -148,93 +137,99 @@ draw = function() {
 };
 
 mouseClicked = function(){
-    //checks to see if a checker is selected
-    for(var i = 0; i<emptySpacesX.length; i++){
-        if(mouseX<=emptySpacesX[i] + 75 && mouseX>=emptySpacesX[i] && mouseY<=emptySpacesY[i] + 75 && mouseY>=emptySpacesY[i] && selected !== null){
-            while (row < 8){
-                //left to right
-                if (column < 8){
-                    if (board[row][column][1] === emptySpacesX[i] && board[row][column][2] === emptySpacesY[i]){
-                        if (selectedColor === "black"){
-                            board[row][column][0] = "D";
-                            for(var d = 0; d<darkCheckersX.length; d++){
-                                var column2 = 0;
-                                var row2 = 0;
-                                while (row2 < 8){
-                                    if (column2 < 8){
-                                        if(darkCheckersX[selected] === board[row2][column2][3] && darkCheckersY[selected] === board[row2][column2][4]){
-                                            println(darkCheckersX[selected]);
-                                            darkCheckersX[selected] = board[row][column][1];
-                                            darkCheckersY[selected] = board[row][column][2];
-                                            selected = null;
-                                            selectedColor = null;
-                                            board[row2][column2][0] = "_";
-                                            println(emptySpacesX[i]);
-                                            emptySpacesX[i] = board[row2][column2][1];
-                                            emptySpacesY[i] = board[row2][column2][2];
-                                            println(emptySpacesX[i]);
-                                            println(darkCheckersX[selected]);
-                                            println(darkCheckersX);
-                                        }
-                                        column2 += 1;
-                                    }
-                                    else{
-                                        row2 += 1;
-                                        column2 = 0;
-                                    }
-                                }
-                            }
-                        }
-                        
-                        else if (selectedColor === "red"){
-                            board[row][column][0] = "L";
-                            for(var l = 0; l<darkCheckersX.length; l++){
-                                var column2 = 0;
-                                var row2 = 0;
-                                while (row2 < 8){
-                                    if (column2 < 8){
-                                        if(lightCheckersX[l] === board[row2][column2][3] && lightCheckersY[l] === board[row2][column2][4]){
-                                            board[row2][column2][0] = "_";
-                                            selected = null;
-                                            selectedColor = null;
-                                        }
-                                        column2 += 1;
-                                    }
-                                    else{
-                                        row2 += 1;
-                                        column2 = 0;
-                                    }
-                                }
-                            }
-                        }
-                        
-                        selectedColor = null;
-                        
-                    }
-                
-                    column += 1;
+    for(var l = 0; l<board.length; l++){
+        for(var i = 0; i<board[l].length; i++){
+            if(mouseX<=board[l][i][1] + 75 && mouseX>=board[l][i][1] && mouseY<=board[l][i][2] + 75 && mouseY>=board[l][i][2] && board[l][i][5] === "light"){
+                if(board[l][i][0] === "_" && selected === true && selectedColor === "black" && board[l][i] === board[selectedColumn+1][selectedRow+1] && turn === "black"){
+                    board[l][i][0] = "D";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "red";
                 }
-            
-            //goes down one and starts at front of row again
-                else{
-                    row += 1;
-                    column = 0;
+                
+                else if(board[l][i][0] === "_" && selected === true && selectedColor === "black" && board[l][i] === board[selectedColumn+1][selectedRow-1] && turn === "black"){
+                    board[l][i][0] = "D";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "red";
+                }
+                
+                else if(board[l][i][0] === "_" && selected === true && selectedColor === "red" && board[l][i] === board[selectedColumn-1][selectedRow+1] && turn === "red"){
+                    board[l][i][0] = "L";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "black";
+                }
+                
+                else if(board[l][i][0] === "_" && selected === true && selectedColor === "red" && board[l][i] === board[selectedColumn-1][selectedRow-1]  && turn === "red"){
+                    board[l][i][0] = "L";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "black";
+                }
+                
+                else if(board[l][i][0] === "L" && selected === true && selectedColor === "black" && board[l][i] === board[selectedColumn+1][selectedRow+1] && board[selectedColumn+2][selectedRow+2][0] === "_" && turn === "black"){
+                    board[selectedColumn+2][selectedRow+2][0] = "D";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    board[selectedColumn+1][selectedRow+1][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "red";
+                }
+                
+                else if(board[l][i][0] === "L" && selected === true && selectedColor === "black" && board[l][i] === board[selectedColumn+1][selectedRow-1] && board[selectedColumn+2][selectedRow-2][0] === "_" && turn === "black"){
+                    board[selectedColumn+2][selectedRow-2][0] = "D";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    board[selectedColumn+1][selectedRow-1][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "red";
+                }
+                
+                else if(board[l][i][0] === "D" && selected === true && selectedColor === "red" && board[l][i] === board[selectedColumn-1][selectedRow+1] && board[selectedColumn-2][selectedRow+2][0] === "_"  && turn === "red"){
+                    board[selectedColumn-2][selectedRow+2][0] = "L";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    board[selectedColumn-1][selectedRow+1][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "black";
+                }
+                
+                else if(board[l][i][0] === "D" && selected === true && selectedColor === "red" && board[l][i] === board[selectedColumn-1][selectedRow-1] && board[selectedColumn-2][selectedRow-2][0] === "_"  && turn === "red"){
+                    board[selectedColumn-2][selectedRow-2][0] = "L";
+                    board[selectedColumn][selectedRow][0] = "_";
+                    board[selectedColumn-1][selectedRow-1][0] = "_";
+                    selected = false;
+                    selectedColumn = null;
+                    selectedRow = null;
+                    turn = "black";
+                }
+                
+                else if(board[l][i][0] === "D"){
+                    selected = true;
+                    selectedColumn = l;
+                    selectedRow = i;
+                    selectedColor = "black";
+                }
+                
+                else if(board[l][i][0] === "L"){
+                    selected = true;
+                    selectedColumn = l;
+                    selectedRow = i;
+                    selectedColor = "red";
                 }
             }
         }
-   }
-   
-   for(var i = 0; i<darkCheckersX.length; i++){
-        if(mouseX<=darkCheckersX[i] + 37.5 && mouseX>=darkCheckersX[i] - 37.5 && mouseY<=darkCheckersY[i] + 37.5 && mouseY>= darkCheckersY[i] - 37.5){
-            selected = i;
-            selectedColor = "black";
-        }
-   }
-   
-   for(var i = 0; i<lightCheckersX.length; i++){
-        if(mouseX<=lightCheckersX[i] + 37.5 && mouseX>=lightCheckersX[i] - 37.5 && mouseY<=lightCheckersY[i] + 37.5 && mouseY>= lightCheckersY[i] - 37.5){
-            selected = i;
-            selectedColor = "red";
-        }
-   }
+    }
 };
